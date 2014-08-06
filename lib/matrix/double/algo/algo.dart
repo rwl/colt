0ed0ff02c641c7500e8dbe41038b762b7973dcd7
Richard Lincoln <r.w.lincoln@gmail.com>
2014-08-06 18:16:57 +0200

    Creating double sub-directory.

---
+++ lib/matrix/double/algo/algo.dart
@@ -0,0 +1,10 @@
+library cern.colt.matrix.algo;
+
+import 'dart:math' as Math;
+import 'dart:async';
+
+import '../../matrix.dart';
+import '../../../util.dart';
+import '../../../function.dart';
+
+part 'property.dart';
