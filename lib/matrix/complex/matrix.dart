1f68a0c6527e7212ce9f2dc551f6430dbd292087
Richard Lincoln <r.w.lincoln@gmail.com>
2015-08-04 23:29:49 +0200

    Full pass of abstract complex matrix and vector.

--- lib/matrix/complex/matrix.dart
+++ lib/matrix/complex/matrix.dart
@@ -1,6 +1,48 @@
 library cern.colt.matrix.complex;
 
-import '../matrix.dart' show AbstractMatrix, AbstractVector;
+import 'dart:math' as Math;
+import 'dart:typed_data';
+//import 'dart:collection' show ListMixin;
+
+import '../matrix.dart'
+    show
+        AbstractMatrix,
+        AbstractVector,
+        checkSize,
+        checkIndex,
+        checkIndexes,
+        vFlip,
+        vPart,
+        vStride,
+        checkShape,
+        checkBox,
+        checkColumn,
+        checkRow,
+        checkColumnIndexes,
+        checkRowIndexes,
+        vStrides,
+        vColumnFlip,
+        vRowFlip,
+        vDice,
+        vBox,
+        setIsNoView,
+        setRows,
+        setColumns;
+
+import '../../function/complex.dart'
+    show ComplexFunction, ComplexComplexFunction;
+
+import '../../function/complex.dart' as cfunc;
+
+import '../double/matrix.dart' show AbstractDoubleVector, AbstractDoubleMatrix;
+
+import 'factory.dart' as cfactory;
+
+import '../../math.dart' show EPSILON;
+
+import 'algo/algo.dart' as cprop;
+
+import '../former.dart';
 
 part 'abstract_complex_vector.dart';
 part 'abstract_complex_matrix.dart';
