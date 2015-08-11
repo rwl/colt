55e1e4c66942f72edc6cd84a8f8166bbd8584c59
Richard Lincoln <r.w.lincoln@gmail.com>
2015-08-11 21:39:27 +0200

    Exporting formatters.

--- lib/colt.dart
+++ lib/colt.dart
@@ -3,3 +3,5 @@ library cern.colt;
 export 'src/matrix/double/matrix.dart';
 export 'src/matrix/int/matrix.dart';
 export 'src/matrix/complex/matrix.dart';
+
+export 'src/matrix/formatter.dart' show DoubleFormatter, IntFormatter;
