6c867555b98db77ccc70035314d299ea10ce623f
Richard Lincoln <r.w.lincoln@gmail.com>
2015-08-11 23:17:36 +0200

    Limit exported API.

--- lib/colt.dart
+++ lib/colt.dart
@@ -1,7 +1,42 @@
 library cern.colt;
 
-export 'src/matrix/double/matrix.dart';
-export 'src/matrix/int/matrix.dart';
-export 'src/matrix/complex/matrix.dart';
+export 'src/matrix/double/matrix.dart'
+    show
+        DoubleVector,
+        DoubleMatrix,
+        DenseDoubleVector,
+        DenseDoubleMatrix,
+        SparseDoubleVector,
+        SparseDoubleMatrix,
+        SparseCCDoubleMatrix,
+        SparseRCDoubleMatrix,
+        DiagonalDoubleMatrix,
+        LargeDoubleMatrix;
+
+export 'src/matrix/int/matrix.dart'
+    show
+        IntVector,
+        IntMatrix,
+        DenseIntVector,
+        DenseIntMatrix,
+        SparseIntVector,
+        SparseIntMatrix,
+        SparseCCIntMatrix,
+        SparseRCIntMatrix,
+        DiagonalIntMatrix,
+        LargeIntMatrix;
+
+export 'src/matrix/complex/matrix.dart'
+    show
+        ComplexVector,
+        ComplexMatrix,
+        DenseComplexVector,
+        DenseComplexMatrix,
+        SparseComplexVector,
+        SparseComplexMatrix,
+        SparseCCComplexMatrix,
+        SparseRCComplexMatrix,
+        DiagonalComplexMatrix,
+        LargeComplexMatrix;
 
 export 'src/matrix/formatter.dart' show DoubleFormatter, IntFormatter;
